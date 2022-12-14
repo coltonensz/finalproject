520b87f6956f8ae8eaf8b57eb6ad0ec1890999a9
coltonensz <coltonensz123@gmail.com>
2022-12-14 12:16:41 -0600

    Added Job model and admin side of things

--- services/models.py
+++ services/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.urls import reverse # Used to generate URLs by reversing the URL patterns
 
-# Create your models here.
+
+class Job(models.Model):
+    """Model representing a job (but not a specific job)."""
+    title = models.CharField(max_length=200)
+
+    description = models.TextField(max_length=1000, help_text='Enter a brief description of the book')
+
+    def __str__(self):
+        """String for representing the Model object."""
+        return self.title
+
+    def get_absolute_url(self):
+        """Returns the URL to access a detail record for this book."""
+        return reverse('job-detail', args=[str(self.id)])
